ea1943998b863f39cf4666c970b25ff668f9283b
userElaina <userelaina@pm.me>
2024-12-25 09:35:56 +0000

    :sparkles: hyperstack

--- cloud/hyperstack/_init.sh
+++ cloud/hyperstack/_init.sh
@@ -1,8 +1,26 @@
-sudo mkdir /mnt/mydisk
-sudo mount /dev/vdb /mnt/mydisk
+sudo chown -R ubuntu:ubuntu /ephemeral
+sudo chmod -R 777 /ephemeral
+
+mkdir -p /ephemeral/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /ephemeral/miniconda3/miniconda.sh
+bash /ephemeral/miniconda3/miniconda.sh -b -u -p /ephemeral/miniconda3
+rm /ephemeral/miniconda3/miniconda.sh
+source /ephemeral/miniconda3/bin/activate
+conda init --all
+
+
 cd /mnt
-sudo chown -R ubuntu mydisk/
-sudo chgrp -R ubuntu mydisk/
+sudo mkdir mydisk
+sudo mount /dev/vdb mydisk
+sudo chown -R ubuntu:ubuntu mydisk
+sudo chmod -R 777 mydisk
+mkdir -p /mnt/mydisk/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /mnt/mydisk/miniconda3/miniconda.sh
+bash /mnt/mydisk/miniconda3/miniconda.sh -b -u -p /mnt/mydisk/miniconda3
+rm /mnt/mydisk/miniconda3/miniconda.sh
+source /mnt/mydisk/miniconda3/bin/activate
+conda init --all
+
 
 mkdir -p ~/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
